956748a117f8ae9d87d591d40fd3224770bbc827
Paulo Valduga <paulo.valduga@hotmail.com>
2021-08-30 12:06:47 -0300

    Contatos mais dinâmicos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/models/contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+    contatos:Contato[] = [
+        {nome: "Paulo",
+        email: "paulo.valduga@hotmail.com",
+        telefones: ['2221-1111', '2222-2222']},
+
+        {nome: "Pedro",
+        email: "pedro.valduga@hotmail.com",
+        telefones: ['3331-1111', '7722-2222']},
+
+        {nome: "Pablo",
+        email: "pablo.valduga@hotmail.com",
+        telefones: ['4441-1111', '6622-2222']},
+
+        {nome: "Paola",
+        email: "paola.valduga@hotmail.com",
+        telefones: ['5551-1111', '5522-2222']},
+
+        {nome: "Paula",
+        email: "paula.valduga@hotmail.com",
+        telefones: ['7751-1111', '5588-2222']}
+    ]
 
   constructor() { }
 
