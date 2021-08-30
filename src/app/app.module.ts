37e3492bc130f3cbe8dfe707f4e5dcee0e0bb357
Paulo Valduga <paulo.valduga@hotmail.com>
2021-08-30 11:30:46 -0300

    Alcancei o professor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ListaDeContatosComponent } from './components/lista-de-contatos/lista-de-contatos.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ListaDeContatosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
